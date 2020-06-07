b340d04f28355355f547d9c1739e42fc0be6f0f2
yejikim22 <yeji.kim22@stu.siskorea.org>
2020-06-07 14:47:34 +0900

    money function

--- app/src/main/java/com/example/microtree/MyDBHelper.java
+++ app/src/main/java/com/example/microtree/MyDBHelper.java
@@ -17,9 +17,9 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
     public void onCreate(SQLiteDatabase db) {
         String sql;
-        sql = "CREATE TABLE donee (username TEXT PRIMARY KEY, name TEXT, phone TEXT, password TEXT)";
+        sql = "CREATE TABLE donee (username TEXT PRIMARY KEY, name TEXT, phone TEXT, password TEXT, money INTEGER, moneyleft INTEGER)";
         db.execSQL(sql);
-        sql = "CREATE TABLE donor (username TEXT PRIMARY KEY, name TEXT, phone TEXT, password TEXT)";
+        sql = "CREATE TABLE donor (username TEXT PRIMARY KEY, name TEXT, phone TEXT, password TEXT, money INTEGER, moneyleft INTEGER)";
         db.execSQL(sql);
     }
 
@@ -30,7 +30,7 @@ public class MyDBHelper extends SQLiteOpenHelper {
     public void insertDonee(String username, String name, String phone, String password) {
         SQLiteDatabase db = getWritableDatabase();
         String sql;
-        sql = "INSERT INTO donee VALUES('"+username+"','"+name+"','"+phone+"', '"+password+"');";
+        sql = "INSERT INTO donee VALUES('"+username+"','"+name+"','"+phone+"', '"+password+"', 0, 0);";
         db.execSQL(sql);
         db.close();
     }
@@ -38,7 +38,7 @@ public class MyDBHelper extends SQLiteOpenHelper {
     public void insertDonor(String username, String name, String phone, String password) {
         SQLiteDatabase db = getWritableDatabase();
         String sql;
-        sql = "INSERT INTO donor VALUES('"+username+"','"+name+"','"+phone+"', '"+password+"');";
+        sql = "INSERT INTO donor VALUES('"+username+"','"+name+"','"+phone+"', '"+password+"', 0, 0);";
         db.execSQL(sql);
         db.close();
     }
@@ -53,6 +53,43 @@ public class MyDBHelper extends SQLiteOpenHelper {
             }
         }
         return res;
+    }
+
+    public int borrowedmoney (String username){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT username, money FROM donee", null);
+        int mon = 0;
+        while(cursor.moveToNext()){
+            if(username.equals(cursor.getString(0))){
+                mon = cursor.getInt(1);
+            }
+        }
+        return mon;
+    }
+
+    public int paybackamount (String username){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT username, moneyleft FROM donee", null);
+        int mon = 0;
+        while(cursor.moveToNext()){
+            if(username.equals(cursor.getString(0))){
+                mon = cursor.getInt(1);
+            }
+        }
+        return mon;
+    }
+
+    public int getmoney (String username){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT username, moneyleft FROM donor", null);
+        int mon = 0;
+        while(cursor.moveToNext()){
+            if(username.equals(cursor.getString(0))){
+                mon = cursor.getInt(1);
+
+            }
+        }
+        return mon;
     }
 
     public boolean searchDonor(String username, String password){
